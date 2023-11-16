a1fff7acacdbb8d9853311f032ed1845a2a31e2c
Jvpl <jvpl1322@gmail.com>
2023-11-16 19:49:49 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,146 @@
+/*
+function renderUpToTwenty(){
+    for(let i = 0; i <= 20; i++){
+        console.log(i);
+    }
+}
+
+renderUpToTwenty();
+*/
+/*
+function tenInTenToAHundred(){
+    for(let i = 10; i < 101; i+=10){
+        console.log(i);
+    }
+}
+
+tenInTenToAHundred();
+*/
+
+/*
+function oddUpToTwenty(){
+    for(let i = 0; i <= 20; i++){
+        if(i % 2 === 1){
+            console.log(i);
+        }
+    }
+}
+
+oddUpToTwenty();
+*/
+
+/*
+function evenUpToTwenty(){
+    for(let i = 0; i < 21; i++){
+        if(i % 2 === 0){
+            console.log(i);
+        }
+    }
+}
+
+evenUpToTwenty();
+*/
+
+/*
+function fromNegativeToPositive(){
+    for(let i = -10; i <= 0; i++){
+        console.log(i);
+    }
+}
+
+fromNegativeToPositive();
+*/
+
+/*
+function inDescendingDirection(){
+    for(let i = 10; i >= 0; i-=1){
+        console.log(i);
+    }
+}
+
+inDescendingDirection();
+*/
+
+/*
+function toSquare(){
+    for(let i = 1; i <= 10; i++){
+        let squareI = i * i;
+        console.log(dobroI);
+    }
+}
+
+toSquare();
+*/
+
+/*
+function quadrado(){
+    for(let i = 15; i <= 200; i++){
+        let resultado = i * i;
+        console.log(resultado);
+    }
+}
+
+quadrado();
+*/
+
+/*
+function somaDosCemNumeros(){
+    let soma = 0;
+    for(let i = 0; i <= 100; i++){
+        soma += i;
+    }
+    return console.log(soma);
+}
+
+somaDosCemNumeros();
+*/
+
+/*
+function menoresQue200(){
+    for(let i = 0; i < 200; i++){
+        if(i % 4 === 0 ){
+            console.log(i);
+        }
+    }
+}
+
+menoresQue200();
+*/
+
+/*
+function mediaAritimetica() {
+    let soma = 0;
+    let numerosPares = 0;
+    for (let i = 50; i <= 70; i++) {
+        if (i % 2 === 0) {
+            soma += i;
+            numerosPares++;
+        }
+    }
+    const resultado = (soma / numerosPares);
+    return `A soma é ${soma} e a média é ${resultado}.`
+}
+
+console.log(mediaAritimetica());
+
+*/
+
+/*
+function fibonacciNumbers() {
+    let atual = 0;
+    let anterior = 0;
+    let proximo = 1;
+        for(let i = 1; i <= 15; i++){
+            console.log(atual);
+            anterior = atual;
+            atual = proximo;
+            proximo = atual + anterior;
+                
+        }
+
+        
+}
+
+
+fibonacciNumbers();
+*/
